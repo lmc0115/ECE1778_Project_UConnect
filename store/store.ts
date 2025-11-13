f27d02dbe03a1bacc9bc3106b538609353d126e5
lmc0115 <muchen.liu1@outlook.com>
2025-11-13 02:19:45 -0500

    use supabase for auth

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,13 @@
-// store/store.ts
 import { configureStore } from "@reduxjs/toolkit";
-import user from "./slices/userSlice";
-import events from "./slices/eventsSlice";
-export const store = configureStore({ reducer: { user, events } });
+import userReducer from "./slices/userSlice";
+import eventsReducer from "./slices/eventsSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    events: eventsReducer,
+  },
+});
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
